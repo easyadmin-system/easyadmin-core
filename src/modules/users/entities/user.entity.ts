4009c146cbaa98c6cb18000f2384a5a6a3567062
Jan Elznic <jan@elznic.com>
2023-11-02 23:41:25 +0100

    Implemented TypeORM and Users module updated to show all users from database

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,1 +1,16 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn, JoinColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true, length: 60 })
+  username: string;
+
+  @Column({ name: 'first_name', length: 50 })
+  firstName: string;
+
+  @Column({ name: 'last_name', length: 50 })
+  lastName: string;
+}
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,15 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-
 @Injectable()
 export class UsersService {
+  constructor(
+    @Inject('USER_REPOSITORY')
+    private userRepository: Repository<User>,
+  ) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll(): Promise<User[]> {
+    return this.userRepository.find();
   }
 
   findOne(id: number) {
