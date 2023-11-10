f4376b4d1479e886b27abf589db865fee087c72c
Jan Elznic <jan@elznic.com>
2023-11-10 10:26:24 +0100

    Users - Login method in controller

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -4,7 +4,8 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { DtoHelperService } from './dto/dto-helper.service';
 import { User } from './entities/user.entity';
-import { UserI } from './users.interfaces';
+import { LoginResponseI, UserI } from './users.interfaces';
+import { LoginUserDto } from './dto/login-user.dto';
 
 const errorNotFound = (error, str) => {
   throw new HttpException({
@@ -77,5 +78,18 @@ export class UsersController {
     } catch (error) { 
       errorNotFound(error, id);
     }
+  }
+
+  @Post('login')
+  async login(@Body() loginUserDto: LoginUserDto): Promise<LoginResponseI> {
+    const userEntity: UserI = await this.dtoHelperService.loginUserDtoToEntity(
+      loginUserDto,
+    );
+    const jwt: string = await this.usersService.login(userEntity);
+    return {
+      access_token: jwt,
+      token_type: 'JWT',
+      expires_in: 10000,
+    };
   }
 }
